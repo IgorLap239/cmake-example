58cf78828883d424fcf1a0ae1db1e2a328140b52
Igor <ilaptev@ya.ru>
2019-08-22 15:04:45 +0300

    final improvements
    1. Files add.h, path.h.in, ver.h.in transferred to the root/src folder
    2. Deleted files path.h, version.h. Now they are created in the root/src folder.
    3. Remove root/include folder.
    4. Removed lines(32,33) from root/src/CMakeLists.txt with private paths to libraries and include folder.

--- include/path.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PATH_H
-#define _PATH_H
-
-const char *g_source_path = "D:/CLionProjects/Final/src";
-const char *g_binary_path = "D:/CLionProjects/Final/cmake-build-debug/src";
-
-#endif //_PATH_H
